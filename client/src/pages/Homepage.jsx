2b65a8f4f46aa18b85c3f4fd2115aecded88fcca
Marina Muñoz <marinacerejidoo@gmail.com>
2024-09-11 22:37:03 +0200

    Started setting up the spotify api routes and added the song search and its functionality to homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Homepage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  //Spotify shows ten tracks with the user's search
+  const [tracks, setTracks] = useState([]);
+
+  const fetchTracks = async (query) => {
+    try {
+      const response = await fetch(
+        `/api/spotify/search?query=${encodeURIComponent(query)}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setTracks(data.tracks.items);
+    } catch (error) {
+      console.error("Error fetching tracks:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTracks(searchQuery);
+  }, [searchQuery]);
   return (
     <>
       <h1>Welcome to our app</h1>
       <label>
         Search here your song:
-        <input type="text" />
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
       </label>
     </>
   );
