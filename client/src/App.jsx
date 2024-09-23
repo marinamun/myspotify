ce0af4d3ed0ba5c378036e423a80d091ed58b07e
Marina Muñoz <marinacerejidoo@gmail.com>
2024-09-23 20:51:56 +0200

    Sign out + Profile page for those logged in only

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SongDetails from "./pages/SongDetails";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/song/:id" element={<SongDetails />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,58 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebaseConfig";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 const Navbar = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the user is logged in with onAuth
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      navigate("/login");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  // Remember this redirects user to login page if they click "profile" while being logged out
+  const handleProfileClick = () => {
+    if (!user) {
+      navigate("/login");
+    }
+  };
+
   return (
     <>
       <p>This will be the navbar</p>
-      <Link to="/">Homepage</Link>
-      <Link to="/signup">Sign up</Link>
-      <Link to="/login">Log in</Link>
+      {/* This will always be visible */} <Link to="/">Homepage</Link>
+      <Link to="/profile" onClick={handleProfileClick}>
+        Profile
+      </Link>{" "}
+      {!user ? (
+        <>
+          {/*   These show when user is NOT logged in */}
+          <Link to="/signup">Sign up</Link>
+          <Link to="/login">Log in</Link>
+        </>
+      ) : (
+        <>
+          {/*   These show when user IS logged in */}
+          <button onClick={handleSignOut}>Sign out</button>
+        </>
+      )}
     </>
   );
 };
+
 export default Navbar;
